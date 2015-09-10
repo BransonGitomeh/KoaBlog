a8196781940299881997405880371169834dade8
BransonGitomeh <sirbranson@outlook.com>
2015-09-10 14:03:11 +0300

    added view single post feature :smile:

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ var render = require('./lib/render');
 //database
 var PostDB = {
   1:{
+    id:1,
     title:'Branson',
     content:'i am an awesome person and i know that'
   },
 
   2:{
+    id:2,
     title:'Caroline',
     content:'i am an awesomest Gf ever... :-P'
   }
@@ -22,13 +24,24 @@ var PostDB = {
 //blogs functions('actions')
 var blog = {
   list:function *(){
+        var posts = PostDB;
         console.log('asked for a list of all blogs');
-        this.body = yield render("list",{PostDB:PostDB});
+        this.body = yield render("list",{posts:posts});
+  }
+}
+
+//methods for a single  post
+var post = {
+  show:function *(id){
+        var post = PostDB[id];
+        console.log('asked for a single post');
+        this.body = yield render("show",{post:post});
   }
 }
 
 //router
-app.use(route.get('/blog', blog.list));
+app.use(route.get('/', blog.list));
+app.use(route.get('/post/:id', post.show));
 
 
 app.listen(3000);
